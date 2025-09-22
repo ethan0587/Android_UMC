20d4979cead211af0cc77d9d42aeaf60f073ff55
ChaeLeeee <cwlee.red25@gmail.com>
2025-09-22 13:47:36 +0900

    setting

--- app/src/main/java/com/example/umc_9th/ui/theme/Theme.kt
+++ app/src/main/java/com/example/umc_9th/ui/theme/Theme.kt
@@ -33,7 +33,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun UMC_8thTheme(
+fun UMC_9thTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
